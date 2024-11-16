c38bcf2b3f3d0a6e502869d0f8022381a8dda59c
Administrator <admin@example.com>
2024-11-16 23:06:36 +0330

    init project

---
+++ src/main/java/com/example/datasetFilter/DatasetFilterApplication.java
@@ -0,0 +1,13 @@
+package com.example.datasetFilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasetFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatasetFilterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/datasetFilter/DatasetFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datasetFilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatasetFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
