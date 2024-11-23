dba8072b97775412d00651e7a6ae8e909c188454
Administrator <admin@example.com>
2024-11-24 01:15:55 +0330

    dev tests for filter api

--- src/main/java/com/example/datasetFilter/entity/TitleEntity.java
+++ src/main/java/com/example/datasetFilter/entity/TitleEntity.java
@@ -2,7 +2,10 @@ package com.example.datasetFilter.entity;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -11,6 +14,9 @@ import java.util.List;
 
 @Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TitleEntity {
 
     @Id
--- src/test/java/com/example/datasetFilter/DatasetFilterApplicationTests.java
+++ src/test/java/com/example/datasetFilter/DatasetFilterApplicationTests.java
@@ -1,44 +1,129 @@
 package com.example.datasetFilter;
 
+import com.example.datasetFilter.entity.NameEntity;
+import com.example.datasetFilter.entity.TitleEntity;
+import com.example.datasetFilter.repository.NameRepository;
+import com.example.datasetFilter.repository.TitleRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.*;
+
+import java.util.List;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DatasetFilterApplicationTests {
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TitleRepository titleRepository;
+    @Autowired
+    private NameRepository nameRepository;
 
 
-	private void filterGetData(String method) throws Exception {
-		mockMvc.perform(get("/api/v1/filter/" + method)
-				)
-				.andExpect(status().isOk());
-	}
+    @BeforeAll
+    void addTestData() {
+        titleRepository.save(TitleEntity.builder()
+                .id("11").primaryTitle("title1").directors(List.of("1")).writers(List.of("2"))
+                .build());
 
-	private void filterPostData(String method, String jsonContent) throws Exception {
-		mockMvc.perform(post("/api/v1/filter/" + method)
-						.contentType(MediaType.APPLICATION_JSON)
-						.content(jsonContent)
-				)
-				.andExpect(status().isOk());
-	}
+        titleRepository.save(TitleEntity.builder()
+                .id("22").primaryTitle("title2").directors(List.of("2")).writers(List.of("1"))
+                .build());
 
 
-	@Test
-	void filterTitlesWhereDirectorAndWriterAreSame() throws Exception {
-		filterGetData("get-director-writer-same"
-		);
-	}
+        nameRepository.save(NameEntity.builder()
+                .id("1").primaryName("Fred Astaire3").deathYear("/N").knownForTitles(List.of("11"))
+                .build());
+        nameRepository.save(NameEntity.builder()
+                .id("2").primaryName("Lauren Bacall3").deathYear("/N").knownForTitles(List.of("11"))
+                .build());
+    }
+
+    private void filterGetDataReturn200(String method) throws Exception {
+        mockMvc.perform(get("/api/v1/filter/" + method)
+                )
+                .andExpect(status().isOk());
+    }
+
+    private void filterPostDataReturn200(String method, String jsonContent) throws Exception {
+        mockMvc.perform(post("/api/v1/filter/" + method)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonContent)
+                )
+                .andExpect(status().isOk());
+    }
+
+    private void filterPostDataReturn400(String method, String jsonContent) throws Exception {
+        mockMvc.perform(post("/api/v1/filter/" + method)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonContent)
+                )
+                .andExpect(status().isBadRequest());
+    }
+
+
+    @Test
+    void filterTitlesWhereDirectorAndWriterAreSameReturn200() throws Exception {
+        filterGetDataReturn200("get-director-writer-same"
+        );
+    }
+
+
+    @Test
+    void filterByActorsReturn200() throws Exception {
+        filterPostDataReturn200("filter-by-actors",
+
+                "{\n" +
+                        "    \"actor1\": \"Fred Astaire3\",\n" +
+                        "    \"actor2\": \"Lauren Bacall3\"\n" +
+                        "}"
+        );
+    }
+
+    @Test
+    void filterByActorsReturn400() throws Exception {
+        filterPostDataReturn400("filter-by-actors",
+
+                "{\n" +
+                        "    \"actor1\": \"Fred Astairrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrre\",\n" +
+                        "    \"actor2\": \"Lauren Bacall\"\n" +
+                        "}"
+        );
+    }
+
+
+    @AfterAll
+    void deleteTestData() {
+        titleRepository.delete(TitleEntity.builder()
+                .id("1").primaryTitle("title1").directors(List.of("1")).writers(List.of("1"))
+                .build());
+
+        titleRepository.delete(TitleEntity.builder()
+                .id("2").primaryTitle("title2").directors(List.of("1")).writers(List.of("1"))
+                .build());
+
+
+        nameRepository.delete(NameEntity.builder()
+                .id("1").primaryName("Fred Astaire3").deathYear("/N").knownForTitles(List.of("1"))
+                .build());
+        nameRepository.delete(NameEntity.builder()
+                .id("2").primaryName("Lauren Bacall3").deathYear("/N").knownForTitles(List.of("1"))
+                .build());
+    }
 
 
 
